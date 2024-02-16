0c687b4d7f77c72b045eff7d754590b2e0c6a2b0
yarenfenci <118660207+YarenFenci@users.noreply.github.com>
2024-02-16 13:27:20 +0300

    Update library_management_system.py

--- library_management_system.py
+++ library_management_system.py
@@ -6,6 +6,33 @@ class Library:
     # destructor to close the books.txt file
     def __del__(self):
         self.file.close()
+     # function to list all books in the file   
+    def list_books(self):
+        with open("books.txt", "r") as file:
+            books_list = file.readlines()
+            # iterate each line on the list
+            for book in books_list:
+                book_info = book.strip().split(",")
+                print(f"Book name: {book_info[0]} | Author: {book_info[1]}")
+    #function to add book
+    def add_book(self):
+        book_title = input("Enter book title: ")
+        book_author = input("Enter book author: ")
+        first_release_year = input("Enter first release year: ")
+        num_of_pages = input("Enter number of pages: ")
+        book_info = f"{book_title},{book_author},{first_release_year},{num_of_pages}\n"
+        with open("books.txt", "a") as file:
+            file.write(book_info)
+    #function to remove book
+    def remove_book(self):
+        book_title_to_delete = input("Enter book title to delete: ")
+        with open("books.txt", "r") as file:
+            books_list = file.readlines()
+        with open("books.txt", "w") as file:
+            for book in books_list:
+                book_info = book.strip().split(",")
+                if book_info[0] != book_title_to_delete:
+                    file.write(book)
 
     # display menu and return user choice
     def menu(self):
